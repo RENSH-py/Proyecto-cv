639d68dc5996ac510439c6a01f192d637d38602b
Ignacio B <ignacio.bravo18@inacapmail.cl>
2024-12-04 23:14:15 -0300

    arreglo angular json

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    backendUrl: 'http://localhost:3000', // Reemplaza con tu API real
+    otherConfig: 'someOtherValue'
+  };
+  
